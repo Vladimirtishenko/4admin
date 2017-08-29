7c2a98c9b24ae49e35f45930ff7b54f046a28a12
Andrei Krasovskii <andrei.k1251@gmail.com>
2017-08-29 14:26:04 +0300

    move table from main page to main_table.jsx

--- assets/js/components/widget/table/main_table.jsx
+++ assets/js/components/widget/table/main_table.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import Header from '../../includes/header.jsx';
 import Sidebar from '../../includes/sidebar.jsx';
+import Cell from './cell.jsx';
 
 class Table extends React.Component {
 
@@ -11,9 +12,32 @@ class Table extends React.Component {
                 <Sidebar />
                 <div className="site-content">
                     <Header />
-                    <div className="container-fluid">
-                        <div className="row">
-                        Table
+                    <div className="container-fluid site-users-table">
+                        <div className="site-users-table__wrapper">
+                            <div className="site-users-table__title">
+                                <h4 className="site-users-table__title-head">All users</h4>
+                                <p className="site-users-table__title-description">Manage them</p>
+                            </div>
+                            <table className="site-users-table__wrapper-table">
+                                <thead>
+                                <tr>
+                                    <td className="site-users-table__wrapper-table-head-data">
+                                        Name
+                                    </td>
+                                    <td className="site-users-table__wrapper-table-head-data">
+                                        E-mail
+                                    </td>
+                                    <td className="site-users-table__wrapper-table-head-data">
+                                        Position
+                                    </td>
+                                    <td className="site-users-table__wrapper-table-head-data">
+                                        Role
+                                    </td>
+                                </tr>
+                                </thead>
+                                <Cell/>
+                            </table>
+                            <div className="clearfix"></div>
                         </div>
                     </div>
                 </div>      {/*site-content*/}
