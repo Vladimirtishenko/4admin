ca673daed4ddacea1414c630fc2e30df11253b57
Владимир Тищенко <valdimirtishenko>
2018-06-05 00:23:55 +0300

    Strict mode is removed

--- modules/menu/model/index.js
+++ modules/menu/model/index.js
@@ -2,6 +2,6 @@ var mongoose = require('../../../lib/mongoose.js');
 var schema = new mongoose.Schema({
     label: {type: String, index: true, required: true, unique : true},
     items: {type: Array, default: []}
-}, {strict: true}, { collection: 'menu' });
+}, { versionKey: false }, { collection: 'menu' });
 // Model
 module.exports = mongoose.model('menu', schema);
